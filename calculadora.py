80b7b6111f962e2bd06899cb1383a9b66fe8321f
AndresCollazos <afcollazosf@unicauca.edu.co>
2025-03-31 22:05:08 -0500

    Se agrega resta

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,6 @@
 numero1 = int(input("Digite el primer número: "))
 numero2 = int(input("Digite el segundo número: "))
 suma = numero1 + numero2
+resta = numero1 - numero2
 print(f"La suma de {numero1} + {numero2} es: {suma}")
+print(f"La resta de {numero1} - {numero2} es: {resta}")
